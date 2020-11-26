6afb632076f1bd364e0f6965af128700822b1c5a
Jose paniagua <joserijo184@outlook.com>
2020-11-26 11:27:09 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase',{ delay: 50 });
+ScrollReveal().reveal('.news-cards', { delay: 50 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 50 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 50 });
